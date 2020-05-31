640ccf8f91e5b1a53ff57d6c57ae8fe8398e6b1b
abhilash4384 <vickyabhilash4384@gmail.com>
2020-05-31 22:27:11 +0530

    Updates

--- src/About/config.js
+++
@@ -1,9 +0,0 @@
-const BioData = {
-    Skills: ['React JS', 'React-Native', 'Angular', 'Node.JS', 'Asp.Net with C# and SQL Server'],
-    bio: `Overall ||EXPERIANCE|| of experience in software development and services.
-    Currently working as a Software developer in Persistent Systems.
-    Skilled Front-end developer with abilities to understand the problem carefully and implement as per expectations, a remarkable knowledge of Back-end and standard knowledge of Database.`,
-    email: 'vickyabhilash4384@gmail.com'
-}
-
-export default BioData;
